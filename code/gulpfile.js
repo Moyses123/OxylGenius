464121610e32cbe4a7b9a3058f331e3c83692179
Gabriel Nascimento <barbosanascimentogabriel@gmail.com>
2017-06-27 22:21:24 -0300

    Pagina index reformulada, owl carrousel e dependencias offline agora

--- code/gulpfile.js
+++ code/gulpfile.js
@@ -7,10 +7,16 @@ var gulp = require('gulp'),
 
 
 gulp.task('css', function () {
-    return gulp.src('css/**/*.css')
+    gulp.src('css/**/*.css')
         .pipe(minifyCSS())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
         .pipe(concat('app.min.css'))
+        .pipe(gulp.dest(''));
+
+    gulp.src('vendor/**/*.css')
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concat('vendor.min.css'))
         .pipe(gulp.dest(''));
 });
 
@@ -19,11 +25,16 @@ gulp.task('js', function () {
         .pipe(concat('app.min.js'))
         //.pipe(uglify())
         .pipe(gulp.dest(''));
+
+    gulp.src('vendor/**/*.js')
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(''));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('js/**/*.js', ['js']), 
-  gulp.watch('css/**/*.css', ['css']);
+gulp.task('watch', function () {
+    gulp.watch('js/**/*.js', ['js']),
+        gulp.watch('css/**/*.css', ['css']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['css', 'js', 'watch']);
